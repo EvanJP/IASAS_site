1b6f7f730e8851b9b1c343b46eca8d7f3e59d007
Evan Peng <Evan Peng>
2016-01-21 22:25:36 +0800

    Added Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,31 @@
 class StaticPagesController < ApplicationController
     def home
-        
     end
     
     def streams
-        
     end
     
     def profiles
-    
     end
     
     def hfame
-        
     end
     
     def userprof
-        
+    end
+    
+    def booster
+    end
+    
+    def guidelines
+    end
+    
+    def introletter
+    end
+    
+    def eventlist 
+    end
+    
+    def schedule
     end
 end
